9961d0dc31ad3d0374e2dc3f6612dccbc1b94fbc
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-10 22:02:38 +0100

    Basic html file

--- index.php
+++ index.php
@@ -10,6 +10,20 @@
     </head>
 
     <body>
-
+        <div id="wrapper">
+            <div id="header"></div>
+            <nav id="topMenu"></nav>
+            <div id="main">
+                <h1>Titel</h1>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                Mauris vel magna. Mauris risus nunc, tristique varius, gravida
+                in, lacinia vel, elit. Nam ornare, felis non faucibus molestie,
+                nulla augue adipiscing mauris, a nonummy diam ligula ut risus.
+                Praesent varius. Cum sociis natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus.</p>
+            </div>
+            <div id="sidebar"></div>
+            <div id="footer">&copy; 2012</div>
+        </div>
     </body>
 </html>
