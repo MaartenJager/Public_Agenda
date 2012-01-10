68d051650a3f7198b1e83e74a49d0fb6318da0a0
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-10 21:54:30 +0100

    added <head> block

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-test
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="utf-8">
+        <title>Paginatitel</title>
+        <link rel="shortcut icon" href="<?php echo $this->baseurl ?>/templates/ipv-ipv/img/favicon.ico" />
+        <meta name="description" content="omschrijving" />
+        <meta name="copyright" content="Copyright (c) 2012" />
+        <meta name="author" content="Projectgroep 1241" />
+    </head>
+
+    <body>
+
+    </body>
+</html>
