8f4e13786a0fcc74a1edaec2be2d35fbfaa2b11f
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-13 00:06:18 +0100

    updated path to php includes

--- contact.php
+++ contact.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="nl">
-    <?php require_once("header.inc"); ?>
+    <?php require_once("inc/header.inc"); ?>
 
     <body>
         <div id="container">
             <div id="header">Pagina titel</div>
-            <?php require_once("nav.inc"); ?>
+            <?php require_once("inc/nav.inc"); ?>
 
             <section id="main">
                 <header class="pageTitle"><h1>Contact</h1></header>
@@ -26,8 +26,8 @@
 
             </section>
 
-            <?php require_once("sidebar.inc"); ?>
-            <?php require_once("footer.inc"); ?>
+            <?php require_once("inc/sidebar.inc"); ?>
+            <?php require_once("inc/footer.inc"); ?>
         </div>
     </body>
 </html>
