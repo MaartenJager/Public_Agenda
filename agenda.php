db9389c45c58c7157c88fb006c6bbd09f1edb8a5
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-13 00:35:43 +0100

    initial commit

---
+++ agenda.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <?php require_once("inc/header.inc"); ?>
+
+    <body>
+        <div id="container">
+            <div id="header">Paginatitel</div>
+            <?php require_once("inc/nav.inc"); ?>
+
+            <section id="main">
+                <header class="pageTitle"><h1>Agenda</h1></header>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                Mauris vel magna. Mauris risus nunc, tristique varius, gravida
+                in, lacinia vel, elit. Nam ornare, felis non faucibus molestie,
+                nulla augue adipiscing mauris, a nonummy diam ligula ut risus.
+                Praesent varius. Cum sociis natoque penatibus et magnis dis
+                parturient montes, nascetur ridiculus mus.</p>
+            </section>
+
+            <?php require_once("inc/sidebar.inc"); ?>
+            <?php require_once("inc/footer.inc"); ?>
+        </div>
+    </body>
+</html>
