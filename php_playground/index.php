3714bff8d2868e0db7cfcd76fe2529ece748395d
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-12 22:59:06 +0100

    created a place to play with some PHP

---
+++ php_playground/index.php
@@ -0,0 +1,9 @@
+<?php require_once("header.inc"); ?>
+<?php require_once("nav.inc"); ?>
+<?php require_once("main.inc"); ?>
+<?php require_once("sidebar.inc"); ?>
+<?php require_once("footer.inc"); ?>
+
+
+
+
