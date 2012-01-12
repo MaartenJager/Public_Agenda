f0ac48fa023829eb1965d5c7171df206188da0ec
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-12 22:34:34 +0100

    escaped ampersands

--- index.php
+++ index.php
@@ -40,8 +40,7 @@
                 Praesent varius. Cum sociis natoque penatibus et magnis dis
                 parturient montes, nascetur ridiculus mus.</p>
             </section>
-            <aside id="sidebar-left">sidebar</aside>
-            <div id="footer">&copy; 2012 <a href="http://validator.w3.org/check?uri=http%3A%2F%2Fwebsec.science.uva.nl%2Fwebdb1241%2F&charset=%28detect+automatically%29&doctype=Inline&group=0">Validate HTML</a> </div>
+            <aside id="sidebar-left">sidebar</aside><div id="footer">&copy; 2012 <a href="http://validator.w3.org/check?uri=http%3A%2F%2Fwebsec.science.uva.nl%2Fwebdb1241%2F&amp;charset=%28detect+automatically%29&amp;doctype=Inline&amp;group=0">Validate HTML</a> </div>
         </div>
     </body>
 </html>
