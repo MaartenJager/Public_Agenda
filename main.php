6b80eac430b5290d0dc5c055ffcd14a4fc0cd420
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-12 23:50:37 +0100

    updated indenting

--- main.php
+++ main.php
@@ -1,20 +1,13 @@
 <!DOCTYPE html>
 <html lang="nl">
-
     <?php require_once("header.inc"); ?>
-
     <body>
         <div id="container">
             <div id="header">Pagina titel</div>
-
             <?php require_once("nav.inc"); ?>
-
             <?php require_once("main.inc"); ?>
-
             <?php require_once("sidebar.inc"); ?>
-
             <?php require_once("footer.inc"); ?>
-
         </div>
     </body>
 </html>
