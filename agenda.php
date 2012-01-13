4f7e1da04332ab47b2580a51f4e7081602e93d30
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-13 22:57:37 +0100

    insered code for agenda

--- agenda.php
+++ agenda.php
@@ -11,10 +11,33 @@
                 <header class="pageTitle"><h1>Agenda</h1></header>
                 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
                 Mauris vel magna. Mauris risus nunc, tristique varius, gravida
-                in, lacinia vel, elit. Nam ornare, felis non faucibus molestie,
-                nulla augue adipiscing mauris, a nonummy diam ligula ut risus.
-                Praesent varius. Cum sociis natoque penatibus et magnis dis
-                parturient montes, nascetur ridiculus mus.</p>
+                in, lacinia vel, elit.</p>
+
+                <div id="agenda">
+                    
+                    <div id="event" class="even" itemscope itemtype="http://data-vocabulary.org/Event">
+                        <div id="dateWrapper">
+                            <div class="day">12</div>
+                            <div class="month">JANUARI</div>
+                        </div>
+                        <div id="commentWrapper">
+                            <a name="0"></a> <!-- HTML anchor  -->
+                            ​<a href="#0" itemprop="url" ><span itemprop="summary">Titel</span></a> <!-- Link to HTML anchor  -->
+                        </div>
+                        <div id="photoWrapper">
+                            <img class="eventPhoto" itemprop="photo" src="img/img.jpg"/>
+                        </div>
+                    </div>
+
+                    <div id="event" class="odd" itemscope itemtype="http://data-vocabulary.org/Event">
+                        <div id="dateWrapper">Hierin de datum</div>
+                        <div id="commentWrapper">Test....</div>
+                        <div id="photoWrapper">
+                            <img class="eventPhoto" itemprop="photo" src="img/img.jpg"/>
+                        </div>
+                    </div>
+                    
+                </div>
             </section>
 
             <?php require_once("inc/sidebar.inc"); ?>
