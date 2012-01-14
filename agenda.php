6d5deef8340af337d10184a7ea6bbc77cfc15b98
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-14 15:04:05 +0100

    added extra html5 microdata tags (start/enddate, location etc.)

--- agenda.php
+++ agenda.php
@@ -23,11 +23,38 @@
                         <div id="commentWrapper">
                             <a name="0"></a> <!-- HTML anchor  -->
                             ​<a href="#0" itemprop="url" ><span itemprop="summary">Titel</span></a> <!-- Link to HTML anchor  -->
+                            <p itemprop="description">My big event is going to be fun. You should come.</p>
+                            <div id="time">
+                                <span itemprop="startDate" datetime="2022-07-04T18:00">July 4th, 2022 at 6:00pm</span> tot
+                                <span itemprop="endDate" datetime="2022-07-04T22:00">July 4th, 2022 at 10:00pm</span>
+                            </div>
+                            <div id="location">@
+                                ​<span itemprop="location" itemscope itemtype="http://data-vocabulary.org/​Organization">
+                                    ​<span itemprop="name">Warfield Theatre</span>
+                                    ​
+                                    <span itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address">
+                                        <span itemprop="street-address">982 Market St</span>, 
+                                        <span itemprop="locality">San Francisco</span>, 
+                                        <span itemprop="region">CA</span>
+                                    </span>
+
+                                    <span itemprop="geo" itemscope itemtype="http://data-vocabulary.org/​Geo">
+                                        <meta itemprop="latitude" content="37.774929" />
+                                        <meta itemprop="longitude" content="-122.419416" />
+                                    </span>
+                                </span>
+                            </div>
                         </div>
                         <div id="photoWrapper">
                             <img class="eventPhoto" itemprop="photo" src="img/img.jpg"/>
                         </div>
                     </div>
+
+
+
+
+
+
                     
                     <div id="event" class="odd" itemscope itemtype="http://data-vocabulary.org/Event">
                         <div id="dateWrapper">
