f72a5b3dc9a5e6f06617f1203c9f6a297c862583
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-15 20:52:06 +0100

    pagina leeggemaakt

--- admin.php
+++ admin.php
@@ -8,37 +8,8 @@
             <?php require_once("inc/nav.inc"); ?>
 
             <section id="main" role="main">
-                <header class="pageTitle"><h1>Evenement toevoegen</h1></header>
+                <header class="pageTitle"><h1>Gebruikers beheren</h1></header>
                 <p>Neem contact met ons op!</p>
-
-                <form name="event-add">
-    		        <label>Naam event</label>
-		            <input name="naamEvent" />		
-						
-			        <label>Datum</label>
-			        <input name="datumEvent" />
-			
-			        <label>Beschrijving van het event</label>
-			        <textarea id="beschrijvingEvent"></textarea>	
-			        		
-			        
-			        <label>Kies de categorieeen die bij het event horen</label>
-			        <ul id="checkbox">
-				        <li><input type="checkbox" name="pop" />pop</li>
-				        <li><input type="checkbox" name="blues" />blues</li>
-				        <li><input type="checkbox" name="metal" />metal</li>
-				        <li><input type="checkbox" name="klassiek" />klassiek</li>
-				        <li><input type="checkbox" name="jazz" />jazz</li>
-				        <li><input type="checkbox" name="hiphop" />hip-hop</li>
-				        <li><input type="checkbox" name="kerkmuziek" />kerkmuziek</li>				
-			        </ul>	
-		
-			        <label>Blader hier voor het toevoegen van een plaatje</label>
-			        <input type="file" name="datafile" />
-			        
-			        <input type="submit" value="Submit" class="button" />
-
-		        </form>   
 
             </section>
 
