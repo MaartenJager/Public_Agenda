06fb559fae7e652dc977745f421b1237b1b69874
MaartenJager <m.h.jager@hotmail.com>
2012-01-16 12:00:58 +0100

    Created file including frame

---
+++ event-accept.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <?php require_once("inc/header.inc"); ?>
+
+    <body>
+        <div id="container">
+            <div id="header" role="banner">Paginatitel</div>
+            <?php require_once("inc/nav.inc"); ?>
+
+            <section id="main" role="main">
+            </section>
+
+            <?php require_once("inc/sidebar.inc"); ?>
+            <?php require_once("inc/footer.inc"); ?>
+        </div>
+    </body>
+</html>
+
