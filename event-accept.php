855df87cae263603ccde704eac1f67d00b5ef6d6
MaartenJager <m.h.jager@hotmail.com>
2012-01-16 12:05:16 +0100

    added table and examples

--- event-accept.php
+++ event-accept.php
@@ -9,6 +9,23 @@
 
             <section id="main" role="main">
                 <header class="pageTitle"><h1>Events Accepteren</h1></header>
+                <table border="1">
+                <tr>
+                    <th>Event title</th>
+                    <th>Created by</th>
+                    <th>Review</th>
+                </tr>
+                <tr>
+                    <td>Grand opening The Roadhouse</td>
+                    <td>Maarten Jager</td>
+                    <td><a href="event-review.php">Review</a></td>
+                </tr>
+                <tr>
+                    <td>2nd party n stuffs</td>
+                    <td>Maarten Jager</td>
+                    <td><a href="event-review.php">Review</a></td>
+                </tr>
+                </table> 
             </section>
 
             <?php require_once("inc/sidebar.inc"); ?>
