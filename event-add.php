b3f7e1cbd49713af9be65cded6cd34a12ef8edf4
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-17 17:17:55 +0100

    form action toegevoegd, uitlijning aangepast

--- event-add.php
+++ event-add.php
@@ -18,7 +18,7 @@
                 <header class="pageTitle"><h1>Evenement toevoegen</h1></header>
                 <p>Wilt u het volgende formulier invullen? Alle velden zijn verplicht. Kies tenminste &#233;&#233;n categorie.</p>
 
-                <form name="event-add">
+                <form name="event-add" action="event-add.php">
     		        <label>Naam evenement</label>
 		        <input name="eventName" placeholder="Voer naam in" autofocus required>		
 						
@@ -40,9 +40,9 @@
                             <checkboxlabel><input id="formCheckbox" type="checkbox" /> Overig</checkboxlabel>
                         </div>
                         
-			<label>Voeg een afbeelding toe</label>
-			<input type="file" name="datafile" />
-			<input id="button" name="submit" type="submit" value="Submit" />
+                        <label>Voeg een afbeelding toe</label>
+                        <input type="file" name="datafile" />
+                        <input id="button" name="submit" type="submit" value="Submit" />
                 </form>
 
             </section>
