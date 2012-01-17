1ff969ac1a2af5c03ac70136a2c791ce4b96c7da
MaartenJager <m.h.jager@hotmail.com>
2012-01-17 16:27:47 +0100

    verneuk file als test

--- printNewEvents.php
+++ printNewEvents.php
@@ -1,29 +1,3 @@
 <?php
 echo "HOI";
-$con = mysql_connect("localhost","webdb1241","qetha8ra");
-if (!$con)
-{
-die('Er is een fout opgetreden. Er kon geen verbinding met de server gemaakt worden.');
-}
-
-mysql_select_db("webdb1241", $con);
-$sql="SELECT * FROM events WHERE approvedBy=0";
-$result=mysql_query($sql);
-mysql_close();
-$num=mysql_numrows($result);
-$i=0;
-echo "werk het al";
-while ($i < $num) {
-echo "hoi";
-$field1=mysql_result($result,$i,"title");
-$field2=mysql_result($result,$i,"createdBy");
-$field3=mysql_result($result,$i,"eventid");
-
-echo "<tr>";
-echo "<td>$field1</td>";
-echo "<td>$field2</td>";
-echo "<td>$field3</td>";
-echo "</tr>";
-$i++;
-}
 ?>
