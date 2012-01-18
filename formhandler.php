34d9390445a2e03fa7adf80eb07d570ebb36d0bb
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-18 14:18:52 +0100

    addUser handler toegevoegd'

--- formhandler.php
+++ formhandler.php
@@ -1,8 +1,25 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        
+        $con = mysql_connect("localhost","webdb1241","qetha8ra");
+        if(!$con)
+        {
+            die('Er is een fout opgetreden. Er kon geen verbinding met de server gemaakt worden.');
+        }
+            
         if (isset($_POST['addUser'])) {
-            echo("Add user form gebruikt");
+            mysql_select_db("webdb1241", $con);
+
+            $sql="INSERT INTO users (name, firstName, email, password, accessLevel)
+            VALUES
+            ('$_POST[name]','$_POST[firstName]','$_POST[email]','$_POST[password]','$_POST[accessLevel]')";
+
+            if (!mysql_query($sql,$con))
+            {
+                die('Er is een fout opgetreden.');
+            }
+
+            echo "De gebruiker is succesvol toegevoegd.";
+            mysql_close($con)
         }
         
         if (isset($_POST['getUser'])) {
