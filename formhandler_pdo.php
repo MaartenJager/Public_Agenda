87822e02a0342d05cd7ba6cff6596bffc5e682e3
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-19 12:23:49 +0100

    named placeholders en databinding gebruikt voor veilige mysql code

--- formhandler_pdo.php
+++ formhandler_pdo.php
@@ -1,41 +1,37 @@
 <?php
-    /* http://net.tutsplus.com/tutorials/php/why-you-should-be-using-phps-pdo-for-database-access/ */
+    /* http://net.tutsplus.com/tutorials/php/why-you-should-be-using-phps-pdo-for-database-access/
+     * http://www.kitebird.com/articles/php-pdo.html
+     * */
     require_once("inc-conf.php");
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         /* Connect to DB */
         require("inc-dbcon.php");
 
-        try{
-            # STH: "Statement Handle"
-            echo("in tweede try..");
-            $STH = $DBH->prepare("INSERT INTO users (name, firstName, email, password, accessLevel)
-                values
-                ('$_POST[name]','$_POST[firstName]','$_POST[email]','$_POST[password]','$_POST[accessLevel]') ");
-
-            $STH->execute();
-        }
-
-        catch(PDOException $e) {
-            echo $e->getMessage();
-        }
-
-
-        /* addUser post action
+        /* addUser post action */
         if (isset($_POST['addUser'])) {
-            echo("addUser form gebruikt");
+            try{
+                echo("in tweede try..");
 
-            $sql="INSERT INTO users (name, firstName, email, password, accessLevel)
-            VALUES
-            ('$_POST[name]','$_POST[firstName]','$_POST[email]','$_POST[password]','$_POST[accessLevel]')";
+                /* Prepare statement */
+                $STH = $DBH->prepare("INSERT INTO users (name, firstName, email, password, accessLevel)
+                    values
+                    (:name, :firstName, :email, :password, :accessLevel) ");
 
-            if (!mysql_query($sql,$con))
-            {
-                die('Er is een fout opgetreden.');
+                /* Prepare data */
+                $STH->bindParam(':name'       , $_POST[name]);
+                $STH->bindParam(':firstName'  , $_POST[firstName]);
+                $STH->bindParam(':email'      , $_POST[email]);
+                $STH->bindParam(':password'   , $_POST[password]);
+                $STH->bindParam(':accessLevel', $_POST[accessLevel]);
+
+                $STH->execute();
             }
 
-            echo "De gebruiker is succesvol toegevoegd.";
-            mysql_close($con);
+            catch(PDOException $e) {
+                echo $e->getMessage();
+            }
+
         }
         */
 
