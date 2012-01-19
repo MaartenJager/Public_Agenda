375af26307a21e886243a8e48f6c65b61d1480cb
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-19 22:30:02 +0100

    aparte div voor gebruiker toevoegen deel

--- admin.php
+++ admin.php
@@ -23,27 +23,29 @@
                     require("inc-dbcon.php");
                 ?>
 
-                <form action="formhandler_pdo.php" method="post">
-                    <label>Voornaam</label>
-                    <input name="firstName" placeholder="Voornaam" autofocus required>
+                <div id="addUser">
+                    <form action="formhandler_pdo.php" method="post">
+                        <label>Voornaam</label>
+                        <input name="firstName" placeholder="Voornaam" autofocus required>
 
-                    <label>Achternaam (inclusief eventuele tussenvoegsels)</label>
-                    <input name="name" placeholder="Achternaam" required>
+                        <label>Achternaam (inclusief eventuele tussenvoegsels)</label>
+                        <input name="name" placeholder="Achternaam" required>
 
-                    <label>Email (tevens de login naam)</label>
-                    <input name="email" type="email" placeholder="Email" required></textarea>
+                        <label>Email (tevens de login naam)</label>
+                        <input name="email" type="email" placeholder="Email" required></textarea>
 
-                    <label>Wachtwoord</label>
-                    <input name="password" placeholder="Wachtwoord" required></textarea>
+                        <label>Wachtwoord</label>
+                        <input name="password" placeholder="Wachtwoord" required></textarea>
 
-                    <label>Toegangsniveau</label>
-                    <select name="accessLevel">
-                        <option value="1">1 (Enkel evenementen toevoegen)</option>
-                        <option value="2">2 (Volledige rechten)</option>
-                    </select>
+                        <label>Toegangsniveau</label>
+                        <select name="accessLevel">
+                            <option value="1">1 (Enkel evenementen toevoegen)</option>
+                            <option value="2">2 (Volledige rechten)</option>
+                        </select>
 
-                    <input id="button" name="addUser" type="submit" value="Voeg gebruiker toe">
-                </form>
+                        <input id="button" name="addUser" type="submit" value="Voeg gebruiker toe">
+                    </form>
+                </div>
 
             </section>
 
