bc997fc094c7226c730eacd806a024ec375d0355
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-20 12:50:41 +0100

    fieldsets verwijderd

--- admin.php
+++ admin.php
@@ -56,27 +56,25 @@
 
                 <div id="addUser">
                     <form action="formhandler_pdo.php" method="post">
-                        <fieldset>
-                            <label>Voornaam</label>
-                            <input name="firstName" placeholder="Voornaam" autofocus required>
+                        <label>Voornaam</label>
+                        <input name="firstName" placeholder="Voornaam" autofocus required>
 
-                            <label>Achternaam (inclusief eventuele tussenvoegsels)</label>
-                            <input name="name" placeholder="Achternaam" required>
+                        <label>Achternaam (inclusief eventuele tussenvoegsels)</label>
+                        <input name="name" placeholder="Achternaam" required>
 
-                            <label>Email (tevens de login naam)</label>
-                            <input name="email" type="email" placeholder="Email" required></textarea>
+                        <label>Email (tevens de login naam)</label>
+                        <input name="email" type="email" placeholder="Email" required></textarea>
 
-                            <label>Wachtwoord</label>
-                            <input name="password" placeholder="Wachtwoord" required></textarea>
+                        <label>Wachtwoord</label>
+                        <input name="password" placeholder="Wachtwoord" required></textarea>
 
-                            <label>Toegangsniveau</label>
-                            <select name="accessLevel">
-                                <option value="1">1 (Enkel evenementen toevoegen)</option>
-                                <option value="2">2 (Volledige rechten)</option>
-                            </select>
+                        <label>Toegangsniveau</label>
+                        <select name="accessLevel">
+                            <option value="1">1 (Enkel evenementen toevoegen)</option>
+                            <option value="2">2 (Volledige rechten)</option>
+                        </select>
 
-                            <input id="button" name="addUser" type="submit" value="Voeg gebruiker toe">
-                        </fieldset>
+                        <input id="button" name="addUser" type="submit" value="Voeg gebruiker toe">
                     </form>
                 </div>
 
