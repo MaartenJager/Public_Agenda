ce2dfabed0fbe5488322fa44e5fba571981579aa
Ben Witzen <ragey@live.nl>
2012-01-20 15:17:16 +0000

    eventadd

--- event-add.php
+++ event-add.php
@@ -16,13 +16,53 @@
                 <p>Wilt u het volgende formulier invullen? Alle velden, op de afbeelding na, zijn verplicht. Kies tenminste &#233;&#233;n categorie.</p>
                 <form name="event-add" action="eventformadd.php" method="post">
                     <label>Naam evenement</label>
-                    <input name="eventName" placeholder="Voer naam in" autofocus required>	
-
-                    <label>Datum</label>
-                    <input type="date" name="eventDate" required>	
+                    <input name="eventName" placeholder="Voer naam in" autofocus required>
 
                     <label>Datum</label>
                     <input name="eventDate" placeholder="bv. 01-01-2012" required>
+                    
+                    <label>Tijd</label>
+                    <select name="eventBeginTimeHours">
+                        <option value="00">00</option>
+                        <option value="01">01</option>
+                        <option value="02">02</option>
+                        <option value="03">03</option>
+                        <option value="04">04</option>
+                        <option value="05">05</option>
+                        <option value="06">06</option>
+                        <option value="07">07</option>
+                        <option value="08">08</option>
+                        <option value="09">09</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                        <option value="13">13</option>
+                        <option value="14">14</option>
+                        <option value="15">15</option>
+                        <option value="16">16</option>
+                        <option value="17">17</option>
+                        <option value="18">18</option>
+                        <option value="19">19</option>
+                        <option value="20">20</option>
+                        <option value="21">21</option>
+                        <option value="22">22</option>
+                        <option value="23">23</option>
+                    </select>
+                    :
+                    <select name="eventBeginTimeMinutes">
+                        <option value="00">00</option>
+                        <option value="05">05</option>
+                        <option value="10">10</option>
+                        <option value="15">15</option>
+                        <option value="20">20</option>
+                        <option value="25">25</option>
+                        <option value="30">30</option>
+                        <option value="35">35</option>
+                        <option value="40">40</option>
+                        <option value="45">45</option>
+                        <option value="50">50</option>
+                        <option value="55">55</option>
+                    </select>
 
                     <label>Beschrijving van het evenement</label>
                     <textarea name="eventDescription" placeholder="Voer beschrijving in" required></textarea>			        		
