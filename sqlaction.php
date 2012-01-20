57a9e030f30581662e557817d88b69579742cba3
MaartenJager <m.h.jager@hotmail.com>
2012-01-20 14:45:54 +0100

    en nog een fix

--- sqlaction.php
+++ sqlaction.php
@@ -10,7 +10,7 @@
         if (isset($_POST['deleteEvent'])) {
             try{
                 //Prepare statement
-                $event_id = &_POST['event_id'];
+                $event_id = $_POST['event_id'];
                 $sth = $dbh->prepare("DELETE FROM events WHERE `id` = " . $event_id);
 
                 $sth->execute();
