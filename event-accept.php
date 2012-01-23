8dcd5a148aef2d4d49e1e9687a8181af57af9ad0
MaartenJager <m.h.jager@hotmail.com>
2012-01-23 15:07:25 +0100

    hoi

--- event-accept.php
+++ event-accept.php
@@ -37,7 +37,7 @@
                             // showing the results
                             while($row = $sth->fetch() ){
                                 echo "<tr>\n";
-                                echo "  <td><input name=\"select\" id=\"formCheckBox\" type=\"checkbox\" /></td>\n";
+                                echo "  <td><input name=\"deleteSelection\" id=\"tableCheckBox\" type=\"checkbox\" /></td>\n";
                                 echo "  <td>" . $row->title . "</td>\n";
                                 echo "  <td>" . $row->location . "</td>\n";
                                 echo "  <td>" . $row->createdBy . "</td>\n";
