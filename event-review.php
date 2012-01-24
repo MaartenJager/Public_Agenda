9d6a8eb24e7971467dec5c40d5edbe19b81971b3
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-24 19:03:20 +0100

    form actie naar GET veranderd, spreekt nieuwe functies in sqldeletes.php aan

--- event-review.php
+++ event-review.php
@@ -15,40 +15,40 @@
     <head>
         <title>Admin - Evenement bewerken</title>
         <?php require_once("inc/header.inc"); ?>
-        
-        <script language="JavaScript">            
+
+        <script language="JavaScript">
             function beginDateAndUnix() {
                 document.getElementById('beginDateUnixCalc').style.display = 'block';
                 document.getElementById('beginDateUnixButton').style.display = 'none';
                 var temp = document.event.beginDate.value;
-                var date = new Date(temp * 1000);       
+                var date = new Date(temp * 1000);
                 document.getElementById('beginDate').innerHTML="Datum: " + date;
             }
-        
+
             function endDateAndUnix() {
                 document.getElementById('endDateUnixCalc').style.display = 'block';
                 document.getElementById('endDateUnixButton').style.display = 'none';
                 var temp = document.event.endDate.value;
-                var date = new Date(temp * 1000);       
+                var date = new Date(temp * 1000);
                 document.getElementById('endDate').innerHTML="Datum: " + date;
             }
-        
+
             function beginDateAutoFill() {
                 var temp = document.event.beginDateReal.value;
                 var date = new Date(temp);
-                var string = date.valueOf() / 1000; 
+                var string = date.valueOf() / 1000;
                 document.event.beginDate.value = string;
             }
-            
+
             function endDateAutoFill() {
                 var temp = document.event.endDateReal.value;
                 var date = new Date(temp);
-                var string = date.valueOf() / 1000; 
+                var string = date.valueOf() / 1000;
                 document.event.endDate.value = string;
             }
         </script>
     </head>
-    
+
     <body>
         <div id="container">
             <div id="header" role="banner"></div>
@@ -62,14 +62,14 @@
                     <input type="hidden" name="event_id" value="<?php $row->id; ?>">
 
                     <label>Naam evenement</label>
-                    <input name="eventName" value="<?php echo $row->title; ?>" required>		
+                    <input name="eventName" value="<?php echo $row->title; ?>" required>
 
                     <label>Begindatum/-tijd</label>
                     <input name="beginDate" placeholder="Unixtimestamp (tmp)" value="<?php echo $row->beginDate; ?>"required>
                     <div id="beginDateUnixButton">
                         <input id="buttonSmall" value="Timestamp Calc" onclick="beginDateAndUnix();" />
                     </div>
-                    
+
                     <div id="beginDateUnixCalc">
                         <p id="beginDate"></p>
                         <p>Nieuwe datum invoeren:</p>
@@ -92,7 +92,7 @@
                     </div>
 
                     <label>Beschrijving van het event</label>
-                    <textarea name="description" placeholder="Voer beschrijving in" required><?php echo $row->description; ?></textarea>			        		
+                    <textarea name="description" placeholder="Voer beschrijving in" required><?php echo $row->description; ?></textarea>
 
                     <label>Kies de categorie&#235;n die bij het event horen</label>
                     <div>
@@ -110,10 +110,11 @@
                     <input type="file" name="datafile" value="<?php echo $row->image; ?>" />
                     <input id="button" name="editEvent" type="submit" value="Submit" />
                 </form>
-                <form action="sqldeletes.php?event_id=<?php echo $row->id; ?>" method="post">
+
+                <form action="sqldeletes.php?action=delete&type=event&id=<?php echo $row->id; ?>" method="get">
                     <input id="button" name="deleteEvent" type="submit" value="Delete" />
                 </form>
-                
+
             </section>
 
             <?php require_once("inc/sidebar.inc"); ?>
