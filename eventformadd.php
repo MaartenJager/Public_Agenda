164afa5a2f2474af6cac20a949cebc6d62cc2781
Igor Willems <igorw@live.nl>
2012-01-24 15:12:45 +0100

    test image

--- eventformadd.php
+++ eventformadd.php
@@ -85,7 +85,7 @@ if (isDatumValid())
 	//Image upload
 	$targetPath = "img/";
 	$imgFileName = mt_rand(0, 99999999);
-	$targetPath = $targetPath . $imgFileName . (file_extension($filename));  
+	$targetPath = $targetPath . $imgFileName . (file_extension($_FILES["file"]));  
 	echo ("<br/>TEST FILE PATH" . $targetPath); 
 	
 	if ((($_FILES["file"]["type"] == "image/gif")
