d7184f3149f06519f680c6a28974bb7ba79508b2
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-25 11:31:00 +0100

    debug

--- agenda.php
+++ agenda.php
@@ -51,7 +51,8 @@
                                         echo("<div class=\"month\">JANUARI</div>\n");
                                     echo("</div>\n");
                                     echo "<div id=\"0\" class=\"comment\">\n" ;
-                                    echo "<a></a> \n" ;
+                                    echo "<a href=\"#\" itemprop=\"url\"><span class=\"summary\" itemprop=\"summary\">Event 1</span></a>" ;
+
 
                         }
                     ?>
