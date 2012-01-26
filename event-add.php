b2b6c6c01483b108135bc21dc0002a9c7fec15f3
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-27 00:27:32 +0100

    tijd per kwartier te selecteren

--- event-add.php
+++ event-add.php
@@ -88,17 +88,9 @@
 
                     <select name="eventEndTimeMinutes">
                         <option value="00">00</option>
-                        <option value="05">05</option>
-                        <option value="10">10</option>
                         <option value="15">15</option>
-                        <option value="20">20</option>
-                        <option value="25">25</option>
                         <option value="30">30</option>
-                        <option value="35">35</option>
-                        <option value="40">40</option>
                         <option value="45">45</option>
-                        <option value="50">50</option>
-                        <option value="55">55</option>
                     </select>
 
                     <label>Kies de locatie voor het event</label>
