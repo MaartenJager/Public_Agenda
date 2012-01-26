dfa12aa39111293ffad928e45123b7887571e5ef
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-27 00:26:07 +0100

    editEvent handler toegevoegd

--- formhandler.php
+++ formhandler.php
@@ -40,7 +40,7 @@
                 $sth = $dbh->prepare("INSERT INTO event (title, beginDate, endDate, location, description, createdBy, image, creationDate, approvedBy)
                     values
                     (:title, :beginDate, :endDate, :location, :description, :createdBy, :image, :creationDate, :approvedBy) ");
-                    
+
                 //Prepare data
                 $sth->bindParam(':title'       , $_POST['title']);
                 $sth->bindParam(':beginDate'   , $_POST['beginDate']);
@@ -62,7 +62,40 @@
             $dbh = null;
         }
 
+        /* addEvent post action */
+        if(isset($_POST['editEvent'])){
 
+            echo "in editEvent";
+
+            /*
+            try{
+                //Prepare statement
+                $sth = $dbh->prepare("INSERT INTO event (title, beginDate, endDate, location, description, createdBy, image, creationDate, approvedBy)
+                    values
+                    (:title, :beginDate, :endDate, :location, :description, :createdBy, :image, :creationDate, :approvedBy) ");
+
+                //Prepare data
+                $sth->bindParam(':title'       , $_POST['title']);
+                $sth->bindParam(':beginDate'   , $_POST['beginDate']);
+                $sth->bindParam(':endDate'     , $_POST['endDate']);
+                $sth->bindParam(':location'    , $_POST['location']);
+                $sth->bindParam(':description' , $_POST['description']);
+                $sth->bindParam(':createdBy'   , $_POST['createdBy']);
+                $sth->bindParam(':image'       , $_POST['image']);
+                $sth->bindParam(':creationDate', $_POST['creationDate']);
+                $sth->bindParam(':approvedBy'  , $_POST['approvedBy']);
+
+                $sth->execute();
+            }
+
+            catch(PDOException $e) {
+                echo $e->getMessage();
+            }
+
+            $dbh = null;
+            */
+
+        }
 
 
 
