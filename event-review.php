288c88a1b66bcdf5a3378ae968d702ad8078c25c
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-27 00:27:55 +0100

    formhandler naar formhandler.php aangepast

--- event-review.php
+++ event-review.php
@@ -26,7 +26,7 @@
             <section id="main">
                 <header class="pageTitle"><h1>Evenement toevoegen</h1></header>
                 <p>Wilt u het volgende formulier invullen? Alle velden, op de afbeelding na, zijn verplicht. Kies tenminste &#233;&#233;n categorie.</p>
-                <form enctype="multipart/form-data" name="event-add" action="eventformadd.php"  method="post">
+                <form enctype="multipart/form-data" name="event-add" action="formhandler.php"  method="post">
                     <label>Naam evenement</label>
                     <input type="text" name="eventName" value="<?php echo $row->title; ?>" autofocus required>
 
