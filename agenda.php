6d481248f95ccf058c00e290f0fea080dee9baaf
Ben Witzen <ragey@live.nl>
2012-01-27 11:04:34 +0000

    item id gedefinieerd in agenda entries

--- agenda.php
+++ agenda.php
@@ -46,12 +46,12 @@
                         while($row = $sth->fetch()) {                          
                             
                             echo("<!-- Begin of item #" . $row->id . "-->\n");
-                            echo("  <div class=\"event even\" itemscope itemtype=\"http://data-vocabulary.org/Event\">\n");
+                            echo("  <div id=\"" . $row->id . "\" class=\"event\" itemscope itemtype=\"http://data-vocabulary.org/Event\">\n");
                             echo("    <div class=\"date\">\n");
                             echo("              <div class=\"day\">" . date("d", $row->beginDate) . "</div>\n");
                             echo("            <div class=\"month\">" . date("F", $row->beginDate) . "</div>\n");
                             echo("        </div>\n");
-                            echo "        <div id=\"" . $row->id . "\" class=\"comment\">\n" ;
+                            echo "        <div class=\"comment\">\n" ;
                             echo "            <a href=\"#\" itemprop=\"url\"><span class=\"summary\" itemprop=\"summary\">". $row->title ."</span></a>\n" ;
                             echo("            <div class=\"description\" itemprop=\"description\">". $row->description ."</div>\n");
                             echo("            <div class=\"meta\">\n");
