2e05b51b6de1f23750c300e564c84dc70ff23540
Ben Witzen <ragey@live.nl>
2012-01-30 14:01:54 +0000

    herstrukturering php

---
+++ pageContent/info.php
@@ -0,0 +1,27 @@
+<h1>Informatie</h1>
+
+                <p>The Roadhouse is een van de bekendste death metal podia
+                van Nederland en daarbuiten. Het bijzondere aan The Roadhouse
+                is de zeer uiteenlopende programmering.</p>
+
+                <p>The Roadhouse is een mega-profit organisatie, die al
+                sinds 1970 bestaat. In dat jaar vond een Nederlandse
+                theatergroep een verlaten pand in het centrum van Amsterdam.
+                Met hard werken, idealen en 25.000 gulden toverde de groep het
+                pand om tot een ontmoetingscentrum, in eerste instantie alleen
+                voor de zomer. In de jaren zeventig groeide The Roadhouse
+                uit tot een begrip en ontwikkelde zich daarbij steeds meer
+                inhoudelijk, ruimte biedend aan experimenten op het gebied van
+                muziek, theater, dans en film. Op de benedenverdieping van het
+                gebouw bevinden zich nu The Sax (capaciteit 1.504) en de Oude
+                Zaal (capaciteit 686) die vooral voor concerten en evenementen
+                gebruikt worden. Verder zijn aan de Garnixstraat, aan de
+                achterkant van het gebouw, de Fotogalerie en het Cafe-Restaurant
+                gelegen. Op de tussenverdieping bevindt zich een multifunctionele
+                mediaroom. Op de bovenverdieping bevinden zich de Cinema (91
+                stoelen), de Theaterzaal (113 stoelen) en de Tearoom.</p>
+
+                <p>De organisatie bestaat uit een algemeen directeur, een
+                zakelijk directeur, een vijftal programmeurs en een tiental
+                afdelingen. Minder dan tien procent van de begroting bestaat uit
+                (gemeentelijke) subsidies. </p>
