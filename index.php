fbec64233bd524603474a34961ca26dab7dc4332
Ben Witzen <ragey@live.nl>
2012-01-30 13:33:30 +0000

    herstrukturering naar index.php

--- index.php
+++ index.php
@@ -1,31 +1,22 @@
 <!DOCTYPE html>
 <html lang="nl">
     <head>
-        <title>The Roadhouse - Home</title>
         <?php require_once("inc/header.inc"); ?>
     </head>
     
     <body>
         <div id="header"></div>
-        
         <?php require_once("inc/nav.inc"); ?>
-        
         <div id="container">
             <section id="main">
-                <h1>Welkom bij The Roadhouse!</h1>
-
-                <p>Hier ben je aan het juiste adres wat betreft concerten, feesten,
-                en meer. Vrijwel iedere dag organiseren wij de meest uiteenlopende
-                parties en voorstellingen. Daarnaast houden wij ook een zeer
-                uitgebreide feest agenda bij. Niet alleen van onze feesten in onze
-                eigen feestpaleis The Roadhouse, maar ook van alle andere grote
-                feesten in het hele land. Op onze site zit je dus altijd goed!</p>
-                
-                <p>Wil je een van onze zalen boeken, of staat jouw evenement niet
-                in onze agenda maar zou je dat wel graag willen? Neem dan even
-                contact met ons op.</p>
-            </section>
-			
+                <?php
+                    if (isset($_GET['page']))
+                        $page = $_GET['page'];
+                    else
+                        $page = "pageContent/home.php";
+                    include $page;
+                ?>
+            </section>         
             <?php require_once("inc/sidebar.inc"); ?>
             <?php require_once("inc/footer.inc"); ?>
 	</div>
---
+++ pageContent/home.php
@@ -0,0 +1,12 @@
+                <h1>Welkom bij The Roadhouse!</h1>
+
+                <p>Hier ben je aan het juiste adres wat betreft concerten, feesten,
+                en meer. Vrijwel iedere dag organiseren wij de meest uiteenlopende
+                parties en voorstellingen. Daarnaast houden wij ook een zeer
+                uitgebreide feest agenda bij. Niet alleen van onze feesten in onze
+                eigen feestpaleis The Roadhouse, maar ook van alle andere grote
+                feesten in het hele land. Op onze site zit je dus altijd goed!</p>
+                
+                <p>Wil je een van onze zalen boeken, of staat jouw evenement niet
+                in onze agenda maar zou je dat wel graag willen? Neem dan even
+                contact met ons op.</p>
