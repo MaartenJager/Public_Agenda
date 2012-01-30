f1e59670bc6cc77927a35f4d734a1e619895d137
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-30 15:05:30 +0100

    rewrite

--- login.php
+++ login.php
@@ -1,66 +1,120 @@
 <?php
+
+    /* Geleend van http://www.phpro.org/tutorials/Basic-Login-Authentication-with-PHP-and-MySQL.html */
+	require("inc-dbcon.php");
+
+	session_start();
+	
+	if(isset( $_SESSION['user_id'] ))
+    {
+        $message = 'Users is already logged in';
+    }
 	
 	
 	$email = $_POST['email'];
 	$password = $_POST['password'];
-	require("inc-dbcon.php");
+	//$password = sha1($password);
+	
+	
+	try
+    {
+        /*** prepare the select statement ***/
+        $sth = $dbh->prepare("SELECT email, password, FROM phpro_users 
+                    WHERE email = :email AND password = :password");
+
+        /*** bind the parameters ***/
+        $sth->bindParam(':email', $email, PDO::PARAM_STR);
+        $sth->bindParam(':password', $password, PDO::PARAM_STR, 40);
+
+        /*** execute the prepared statement ***/
+        $sth->execute();
+
+        /*** check for a result ***/
+        $user_id = $sth->fetchColumn();
+
+        /*** if we have no result then fail boat ***/
+        if($user_id == false)
+        {
+                $message = 'Login Failed';
+        }
+        /*** if we do have a result, all is well ***/
+        else
+        {
+                /*** set the session user_id variable ***/
+                $_SESSION['user_id'] = $user_id;
+
+                /*** tell the user we are logged in ***/
+                $message = 'You are now logged in';
+        }
+
+
+    }
+    catch(Exception $e)
+    {
+        /*** if we are here, something has gone wrong with the database ***/
+        $message = 'We are unable to process your request. Please try again later"';
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	/*
-	function SetEmail($email)
-    {
-        return mysql_real_escape_string($email);
-    }
+			
+	$sth = $dbh->prepare("SELECT email, password FROM users
+	WHERE email = :email AND password = :password
+	");
+	$sth->bindParam(':email', $email);
+	$sth->bindParam(':password', $password);
+	$sth->execute();
 	
-	function SetPassword($password)
-    {
-        return sha1(mysql_real_escape_string($password));
-    }
-    */
-    /*
-	function CheckLogin()
-	{
-	
+	//er is een correcte login en password combinatie
+	if($sth->rowCount() > 0 )
+	{    
+		echo "er is een combo?<br />";
+		$_SESSION['email'] = $email;
 	}
-	*/	session_start();
-		
-				
-		$sth = $dbh->prepare("SELECT email, password FROM users
-		WHERE email = :email AND password = :password
-		");
-		$sth->bindParam(':email', $email);
-		$sth->bindParam(':password', $password);
-		$sth->execute();
-		
-		//er is een correcte login en password combinatie
-		if($sth->rowCount() > 0 )
-		{    
-			echo "er is een combo?<br />";
-			$_SESSION['email'] = $email;
-		}
     /*
-	function Logout()
-	{
-		session_start();
-		unset($_SESSION['userName']);
-	}
+	    function Logout()
+	    {
+		    session_start();
+		    unset($_SESSION['userName']);
+	    }
+        */
+
+    /*
+    SetEmail($_POST['userName']);
+    SetPassword($_POST['userPass']);
     */
 
-/*
-SetEmail($_POST['userName']);
-SetPassword($_POST['userPass']);
-*/
 
+    //CheckLogin(); 
 
-//CheckLogin(); 
-
-//als combo goed is ga naar login 
-if(isset($_SESSION['email']))
-{
-	echo "user {$_SESSION['email']} loged in  ";
-}
-else
-{
-	echo "niet ingelogd LUL";
-}
-$dbh = null;
+    //als combo goed is ga naar login 
+    /*
+    if(isset($_SESSION['email']))
+    {
+	    echo "user {$_SESSION['email']} loged in  ";
+    }
+    
+    else
+    {
+	    echo "niet ingelogd LUL";
+    }
+    */
+    $dbh = null;
 ?>
