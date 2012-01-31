b09cdb83d8152f40be48aff8576479fddd6ece9b
Ben Witzen <ragey@live.nl>
2012-01-31 22:48:52 +0100

    restore old state, try again

--- pageContent/agenda.php
+++ pageContent/agenda.php
@@ -39,47 +39,41 @@
                 <div id="agenda">
                     <?php
                         // showing the results
-                        $counter = 0;
-                        while($row = $sth->fetch()) {                          
-                            $counter = $counter + 1;
+                        while($row = $sth->fetch()) {
+                            
                             echo("<!-- Begin of item #" . $row->id . "-->\n");
-                            echo("  <div id=\"" . $row->id . "\" class=\"event\" itemscope itemtype=\"http://data-vocabulary.org/Event\">\n");
-                            if ($counter % 2)
-                                echo("  <div class=\"odd\">\n");
-                            else
-                                echo("  <div class=\"even\">\n");
-                            echo("    <div class=\"date\">\n");
-                            echo("              <div class=\"day\">" . date("d", $row->beginDate) . "</div>\n");
-                            echo("            <div class=\"month\">" . date("F", $row->beginDate) . "</div>\n");
-                            echo("        </div>\n");
-                            echo "        <div class=\"comment\">\n" ;
-                            echo "            <a href=\"#" . $row->id . "\" onclick=\"expandEntry('" . $row->id . "');\" itemprop=\"url\"><span class=\"summary\" itemprop=\"summary\">". $row->title ."</span></a> <a href=\"index.php?page=event-review&id=" . $row->id . "\">Edit</a>\n" ;
-                            echo("            <div id=\"description" . $row->id . "\"class=\"description\" itemprop=\"description\">". $row->description ."</div>\n");
-                            echo("            <div class=\"meta\">\n");
-                            echo("                <span itemprop=\"startDate\" datetime=\"2022-07-04T18:00\">" . date("d-m-Y H:i:s", $row->beginDate) . "</span> tot\n");
-                            echo("                <span itemprop=\"endDate\" datetime=\"2022-07-04T22:00\">" . date("d-m-Y H:i:s", $row->endDate) . "</span>\n");
-                            echo("            </div>\n");
-                            echo("            <div class=\"meta\">@\n");
-                            echo("                ?<span itemprop=\"location\" itemscope itemtype=\"http://data-vocabulary.org/?Organization\">\n");
-                            echo("                    " . $row->locationName ."(\n");
-                            echo("?                    <span itemprop=\"name\">the Roadhouse</span>\n");
-                            echo("                    \n");
-                            echo("                    <span itemprop=\"address\" itemscope itemtype=\"http://data-vocabulary.org/Address\">\n");
-                            echo("                    <span itemprop=\"street-address\">Science Park 904</span>,\n");
-                            echo("                        <span itemprop=\"locality\">Amsterdam</span>,\n");
-                            echo("                        <span itemprop=\"country-name\">Nederland</span>\n");
-                            echo("                    </span>)\n");
-                            echo("                \n");
-                            echo("                <span itemprop=\"geo\" itemscope itemtype=\"http://data-vocabulary.org/?Geo\">\n");
-                            echo("                        <meta itemprop=\"latitude\" content=\"52.354496\" />\n");
-                            echo("                        <meta itemprop=\"longitude\ content=\"4.954206\" />\n");
-                            echo("                    </span>\n");
-                            echo("                </span>\n");
-                            echo("            </div>\n");
-                            echo("        </div>\n");
-                            echo("            <img itemprop=\"photo\" src=\"". $row->image . "\"/>\n");
-                            echo("         </div>\n");
-                            echo("    </div>\n");
+                            echo(" <div id=\"" . $row->id . "\" class=\"event\" class=\"odd\" itemscope itemtype=\"http://data-vocabulary.org/Event\">\n");
+                            echo(" <div class=\"date\">\n");
+                            echo(" <div class=\"day\">" . date("d", $row->beginDate) . "</div>\n");
+                            echo(" <div class=\"month\">" . date("F", $row->beginDate) . "</div>\n");
+                            echo(" </div>\n");
+                            echo " <div class=\"comment\">\n" ;
+                            echo " <a href=\"#" . $row->id . "\" onclick=\"expandEntry('" . $row->id . "');\" itemprop=\"url\"><span class=\"summary\" itemprop=\"summary\">". $row->title ."</span></a> <a href=\"index.php?page=event-review&id=" . $row->id . "\">Edit</a>\n" ;
+                            echo(" <div id=\"description" . $row->id . "\"class=\"description\" itemprop=\"description\">". $row->description ."</div>\n");
+                            echo(" <div class=\"meta\">\n");
+                            echo(" <span itemprop=\"startDate\" datetime=\"2022-07-04T18:00\">" . date("d-m-Y H:i:s", $row->beginDate) . "</span> tot\n");
+                            echo(" <span itemprop=\"endDate\" datetime=\"2022-07-04T22:00\">" . date("d-m-Y H:i:s", $row->endDate) . "</span>\n");
+                            echo(" </div>\n");
+                            echo(" <div class=\"meta\">@\n");
+                            echo(" ?<span itemprop=\"location\" itemscope itemtype=\"http://data-vocabulary.org/?Organization\">\n");
+                            echo(" " . $row->locationName ."(\n");
+                            echo("? <span itemprop=\"name\">the Roadhouse</span>\n");
+                            echo(" \n");
+                            echo(" <span itemprop=\"address\" itemscope itemtype=\"http://data-vocabulary.org/Address\">\n");
+                            echo(" <span itemprop=\"street-address\">Science Park 904</span>,\n");
+                            echo(" <span itemprop=\"locality\">Amsterdam</span>,\n");
+                            echo(" <span itemprop=\"country-name\">Nederland</span>\n");
+                            echo(" </span>)\n");
+                            echo(" \n");
+                            echo(" <span itemprop=\"geo\" itemscope itemtype=\"http://data-vocabulary.org/?Geo\">\n");
+                            echo(" <meta itemprop=\"latitude\" content=\"52.354496\" />\n");
+                            echo(" <meta itemprop=\"longitude\ content=\"4.954206\" />\n");
+                            echo(" </span>\n");
+                            echo(" </span>\n");
+                            echo(" </div>\n");
+                            echo(" </div>\n");
+                            echo(" <img itemprop=\"photo\" src=\"". $row->image . "\"/>\n");
+                            echo(" </div>\n");
                             echo("<!-- End of item -->\n");
                         }
                     ?>
