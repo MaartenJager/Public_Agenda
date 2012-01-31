54516936e645765bd1893a484fadf5a6fec4ea01
David van Schoorisse <d.schoorisse@gmail.com>
2012-01-31 15:39:35 +0100

    initial commit

---
+++ tmpTestPriv.php
@@ -0,0 +1,17 @@
+<?php
+    if (isset( $_SESSION['accessLevel'] )){
+    	echo "accessLevel is set";
+    	
+        if (($_SESSION['accessLevel'] == 2) || ($_SESSION['accessLevel'] == 1)){
+            echo "accessLevel heeft waarde 1 of 2" ;
+        }
+        else
+        {   
+            echo "accessLevel heeft niet waarde 1 of 2" ;
+        }
+    }
+    
+    else{
+        echo "accessLevel is not set";
+    }
+?>
