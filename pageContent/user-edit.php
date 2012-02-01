2b6bb2325c689b5ffb6e1c58086e4b4eeee89f1c
David van Schoorisse <d.schoorisse@gmail.com>
2012-02-01 13:35:59 +0100

    elseif toegevoegd

--- pageContent/user-edit.php
+++ pageContent/user-edit.php
@@ -1,10 +1,13 @@
 <?php
+    session_start();
     if (isset( $_SESSION['accessLevel'] ))
     {
 ?>
 
-    <?php if ($_SESSION['accessLevel'] == 1 || $_SESSION['accessLevel'] == 2): ?>
-    Ingelogd met level 1 of 2
+    <?php if ($_SESSION['accessLevel'] == 1: ?>
+    Ingelogd met level 1
+    <? elseif ($_SESSION['accessLevel'] == 2): ?>
+    Ingelogd met level 2
     <?php endif; ?>
 
 <?php   
