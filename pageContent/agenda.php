8bcdf87e274916073ce254c19879306ecd55687b
Ben Witzen <ragey@live.nl>
2012-02-01 23:14:57 +0100

    agenda displays genres

--- pageContent/agenda.php
+++ pageContent/agenda.php
@@ -71,6 +71,14 @@
     $sth->execute();
     $booleanArray = array_fill(0, 8, FALSE);
 
+    // Todo:
+    // - Get total amount of genres available, preset strings to their genre names (currently hardcoded)
+    $i=1;
+    while( $i < 9 ) {
+        $i++;
+        $tempInt=$i-1;
+        $booleanArray[$tempInt] = TRUE;
+    }
 ?>
 
 <script language="JavaScript">
@@ -83,15 +91,6 @@
         }
     }
 </script>
-
-    while( $row2 = $sth->fetch() ) {
-        $i=1;
-        while( $i != $row2->genreId ) {
-            $i++;
-        }
-        $tempInt=$i-1;
-        $booleanArray[$tempInt] = TRUE;
-    }
 
                 <h1>Agenda</h1>
                 <p>Alle aankomende evenementen zijn opgenomen in onze agenda. Zoekt u een bepaald evenement?
