c72d83e284b2e8ac72908fd7e4b78e44d7d874dc
David van Schoorisse <d.schoorisse@gmail.com>
2012-02-01 23:01:24 +0100

    meer debug echos toegevoegd"

--- pageContent/user-edit.php
+++ pageContent/user-edit.php
@@ -3,6 +3,7 @@
 <?php if (isset( $_SESSION['accessLevel'] )): ?>
     <?php if ($_SESSION['accessLevel'] == 1): ?>
         <?php
+            echo "<p>DBG: lvl1, (GEEN id in URL):</p>"; 
             echo "<p>Hieronder is uw profiel in te zien/te bewerken:</p>";
             $id = $_SESSION['userId'];
         ?>
@@ -11,12 +12,14 @@
     <?php elseif ($_SESSION['accessLevel'] == 2): ?>
         <?php
             //Rechten om iemand anders zijn profiel aan te passen, haal id uit URL
-            if (isset( $GET['id']) ){                
-            echo "<p>Hieronder is het opgevraagde profiel in te zien/te bewerken:</p>";
+            if (isset( $GET['id']) ){
+                echo "<p>DBG: lvl2, id in URL:</p>";                
+                echo "<p>Hieronder is het opgevraagde profiel in te zien/te bewerken:</p>";
                 $id = strip_tags($_GET['id']);
             }
             //Indien geen id meegegeven in URL geef dan huidige ingelogd gebruiker in
-            else{                   
+            else{
+                echo "<p>DBG: lvl2, GEEN id in URL:</p>";   
                 echo "<p>Hieronder is uw profiel in te zien/te bewerken:</p>";
                 $id = $_SESSION['userId'];
             }
