053a8ad561a2b0cdd3bee13534451dc7c522e88b
David van Schoorisse <d.schoorisse@gmail.com>
2012-02-01 14:48:37 +0100

    doorsturen uitgeschakeld

--- logout.php
+++ logout.php
@@ -3,5 +3,5 @@
     unset($_SESSION['user_id']);
     session_destroy();
     echo 'You\'re logged out succesfully!';
-    header("Location: /webdb1241/index.php");
+    //header("Location: /webdb1241/index.php");
 ?>
