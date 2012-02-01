15b5498602326d222f60151c7631fb55bb132ad4
David van Schoorisse <d.schoorisse@gmail.com>
2012-02-01 16:04:49 +0100

    dbg

--- pageContent/user-edit.php
+++ pageContent/user-edit.php
@@ -1,31 +1,36 @@
 <h1>Profiel</h1>
 
 <?php if (isset( $_SESSION['accessLevel'] )): ?>
+    <?php if ($_SESSION['accessLevel'] == 1): ?>
+        <p>Hieronder worden uw gegevens weergegeven zoals deze bij ons bekend zijn:</p>
+        <?php
+            //Rechten om alleen eigen profiel aan te passen
+            $id = $_SESSION['userId'];
+        ?>
+       
+        
+    <?php elseif ($_SESSION['accessLevel'] == 2): ?>
+        <p>Hieronder is het opgevraagde profiel in te zien/te bewerken:</p>
+        <?php
+            //Rechten om iemand anders zijn profiel aan te passen, haal id uit G
+            $id = strip_tags($_GET['id']);
+        ?>
+    <?php else: ?>
+        Wel ingelogd, geen rechten!?
+    <?php endif; ?>
+
     <?php
-        /* Fetch event with ID.. */
+    /* Fetch event with ID.. */
         require_once("inc-conf.php");
         require("inc-dbcon.php");
 
         $sth = $dbh->prepare("SELECT * FROM users WHERE id=:id");
-        $id = strip_tags($_GET['id']);
         $sth->bindParam(':id', $id);
         $sth->setFetchMode(PDO::FETCH_OBJ);
         $sth->execute();
     ?>
 
-    <?php
-        //Level 1: Mag alleen zijn eigen gegevens aanpassen
-        if ($_SESSION['accessLevel'] == 1):
-    ?>
-       
-        
-    <?php elseif ($_SESSION['accessLevel'] == 2): ?>
-        <!--- ingelogd met level 3 -->
-    <?php else: ?>
-        Wel ingelogd, geen rechten!?
-    <?php endif; ?>
-
-    <p>Hieronder worden uw gegevens weergegeven zoals deze bij ons bekend zijn:</p>
+    
     <p>
         id: <?php echo $id; ?>
         <?php
