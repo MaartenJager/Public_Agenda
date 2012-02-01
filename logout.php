4e9cfc9326fe3ff9c7ba9a3bd3cdd70d2a402fc3
David van Schoorisse <d.schoorisse@gmail.com>
2012-02-01 14:45:33 +0100

    overbodige zooi verwijderd

--- logout.php
+++ logout.php
@@ -1,10 +1,7 @@
 <?php
-
-    //function Logout()
-    //{
-        session_start();
-        unset($_SESSION['user_id']);
-        session_destroy();
-        echo 'You\'re logged out succesfully!';
-        header("Location: /webdb1241/index.php");
+    session_start();
+    unset($_SESSION['user_id']);
+    session_destroy();
+    echo 'You\'re logged out succesfully!';
+    header("Location: /webdb1241/index.php");
 ?>
