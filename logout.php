f1b5e1a0cc800562c211c97ef859d90e53bb5355
Igor Willems <igorw@live.nl>
2012-02-01 13:18:53 +0100

    login logout verbeter en $_SESSION['userId'] aangemaakt

--- logout.php
+++ logout.php
@@ -5,5 +5,6 @@
         session_start();
         unset($_SESSION['user_id']);
         session_destroy();
-    //}
+        echo 'You\'re logged out succesfully!';
+        echo '<a href="http://websec.science.uva.nl/webdb1241">Click here to return to the main page</a>';
 ?>
