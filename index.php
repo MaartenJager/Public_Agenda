7821827181db018fdfe7b50f92effb56d0621013
David van Schoorisse <d.schoorisse@gmail.com>
2012-02-01 22:24:14 +0100

    code rewrite

--- index.php
+++ index.php
@@ -7,18 +7,17 @@
    
     <body>
 				<div id="header">
-		<?php
-		if(isset( $_SESSION['email'] ))
-		{
-			echo '
-					<div id="loginStatus">
-						<div id="loginStatusContent">
-							<p>Ingelogd als ' . $_SESSION['email'] . ' <a href="index.php?page=user-edit">Wachtwoord wijzigen</a></p>
-						</div>
-					</div>
-				';
-		}
-		?>
+
+            
+                    <?php //Controleer of de gebruiker ingelogd is; zoja, geef dit weer in container ?>
+                    <?php if(isset( $_SESSION['email'] )): ?>
+                        <div id="loginStatus">
+                            <div id="loginStatusContent">
+                                <p>Ingelogd als <?php $_SESSION['email'] ?> <a href="index.php?page=user-edit">Wachtwoord wijzigen</a></p>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+        
 				</div>
 	<?php require_once("inc/nav.inc");?>
 		
