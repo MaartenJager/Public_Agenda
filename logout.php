aff85c9e08484f276b00f3cbf773710579823729
David van Schoorisse <d.schoorisse@gmail.com>
2012-02-01 14:43:30 +0100

    doorsturen naar GOEDE adres

--- logout.php
+++ logout.php
@@ -6,5 +6,5 @@
         unset($_SESSION['user_id']);
         session_destroy();
         echo 'You\'re logged out succesfully!';
-        header("Location: ./");
+        header("Location: /webdb1241/index.php");
 ?>
