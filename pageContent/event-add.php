8d4bf74b9ef3a62428fbf448b2cf561c96ba6bf0
Igor Willems <igorw@live.nl>
2012-02-02 14:42:08 +0100

    javascript fix

--- pageContent/event-add.php
+++ pageContent/event-add.php
@@ -7,7 +7,7 @@ if (isset( $_SESSION['accessLevel'] ))
 		echo '
 		<header class="pageTitle"><h1>Evenement toevoegen</h1></header>
 						<p>Wilt u het volgende formulier invullen? Alle velden, op de afbeelding na, zijn verplicht. Kies tenminste &#233;&#233;n categorie.</p>
-						<form enctype="multipart/form-data" name="event-add" action="formhandler.php"  method="post">
+						<form enctype="multipart/form-data" name="event-add" action="formhandler.php"  method="post" onSubmit="checkCheckboxes();">
 							<label>Naam evenement</label>
 							<input type="text" name="eventName" placeholder="Voer naam in" autofocus required>
 		
@@ -117,13 +117,14 @@ if (isset( $_SESSION['accessLevel'] ))
 							<div id="checkbox_below">
 								 <label>Voeg een afbeelding toe</label>
 								 <input type="file" name="file" id ="file" />
-								 <input id="button" name="addEvent" type="submit" value="Submit" onclick="checkCheckBoxes()" />
+								 <input id="button" name="addEvent" type="submit" value="Submit" />
 							</div>
 						</form>
 						
 						<script>
 							function checkCheckBoxes()
 							{
+							alert ("werkt test");
 								var checkboxes = document.event-add.getElementsByTagName(\'input\');
 								for(var i = 0; i < checkboxes.length; i++)
 								{
