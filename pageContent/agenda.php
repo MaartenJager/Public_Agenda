424fec40e48a19ccbde6546d4404b801aed2d929
MaartenJager <m.h.jager@hotmail.com>
2012-02-03 14:48:52 +0100

    finalize

--- pageContent/agenda.php
+++ pageContent/agenda.php
@@ -123,8 +123,8 @@
     if($offsetUsed){
         $offset = strip_tags($_GET['offset']);
         $offsetMax = $offset + 10;
-        $sth->bindParam(:offset, $offset);
-        $sth->bindParam(:offsetMax, $offsetMax);
+        $sth->bindParam(':offset', $offset);
+        $sth->bindParam(':offsetMax', $offsetMax);
     }
 
     $sth->setFetchMode(PDO::FETCH_OBJ);
