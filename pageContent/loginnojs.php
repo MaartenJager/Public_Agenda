b25861b2c18349d2150121dc3c8d2a8f7e6b04cf
Igor Willems <igorw@live.nl>
2012-02-03 15:40:42 +0100

    test java

---
+++ pageContent/loginnojs.php
@@ -0,0 +1,11 @@
+ 
+        <div id="loginBox">
+            <div id="loginBoxContent">
+                <form name="login" action="login.php" method="post">
+                    <label>Inloggen</label>
+                    <input name="email" placeholder="Uw email" required>
+                    <input name="password" type="password" placeholder="Uw wachtwoord" required>
+                    <input id="button" name="login" type="submit" value="Login" />
+                </form>
+            </div>
+        </div>    
